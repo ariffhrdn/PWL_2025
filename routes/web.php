f58eb4e3f25ee63b111c4b5df1caf61bbf5a6067
= <=>
2025-02-18 14:01:58 +0700

    perubahan pada jobsheet 2/ praktikum routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,45 @@
 
 use Illuminate\Support\Facades\Route;
 
+ Route::get('/hello', function () {
+    return 'Hello World';
+ });
+
+ Route::get('/world', function () {
+    return 'World';
+ });
+
+ Route::get('/', function() {
+    return 'Selamat Datang';
+ });
+
+ Route::get('/about', function() {
+    return '2341760192/Muhammad Arif Fahrudin';
+ });
+ 
+//  Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+//     });
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/article/{ID}', function($ID) {
+    return 'Halaman Artikel dengan ID-'.$ID;
+});
+    
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+    
+    
+    
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +52,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+
