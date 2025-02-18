504003706071697bebc0c581a243aeeb914466c3
= <=>
2025-02-18 15:24:21 +0700

    perubahan pada jobsheet 2/ praktikum view

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,13 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
+// Route::get('/greeting', function () {
+// 	return view('blog.hello', ['name' => 'Arif']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+
     
 
 /*
